7194b8c35161b2ff71ccea98b730773cd97a4cc3
jls83 <josephlsanders@gmail.com>
2024-02-06 21:27:57 -0500

    Change CLI to use interface name, other clean up

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,70 @@
+use std::collections::HashMap;
 use std::net::{SocketAddr, SocketAddrV4};
 use std::sync::Arc;
 
 use clap::Parser;
 
+use network_interface::{Addr, NetworkInterface, NetworkInterfaceConfig};
 use tokio::io;
 use tokio::net::UdpSocket;
 use tokio::sync::broadcast;
+
+const DATAGRAM_SIZE: usize = 65_535;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     // TODO: Ipv4Addr, Port separate
+    // #[arg(short, long, value_delimiter = ',')]
+    // addresses: Option<Vec<SocketAddrV4>>,
+    #[arg(short, long)]
+    port: Option<u16>,
+
     #[arg(short, long, value_delimiter = ',')]
-    addresses: Option<Vec<SocketAddrV4>>,
+    interfaces: Option<Vec<String>>,
+}
+
+fn get_interface_map() -> HashMap<String, NetworkInterface> {
+    NetworkInterface::show()
+        .unwrap()
+        .iter()
+        .map(|interface| (interface.name.to_string(), interface.clone()))
+        .collect()
 }
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    let interface_map: HashMap<String, NetworkInterface> = get_interface_map();
+
     let args = Args::parse();
 
     // TODO: Better error handling
-    let speak_addresses = args.addresses.expect("Wrong speak_addresses config");
+
+    let port = args.port.expect("Wrong port config");
+    let interfaces = args.interfaces.expect("Wrong interfaces config");
+
+    let speak_addresses: Vec<SocketAddrV4> = interfaces
+        .iter()
+        .flat_map(|interface_name| {
+            interface_map
+                .get(interface_name)
+                .unwrap()
+                .addr
+                .iter()
+                .filter_map(|x| match x {
+                    Addr::V4(s) => Some(SocketAddrV4::new(s.ip, port)),
+                    _ => None,
+                })
+        })
+        .collect();
+
+    if speak_addresses.len() == 0 {
+        panic!("No interfaces to listen on");
+    }
 
     let (tx, _rx) = broadcast::channel::<(Vec<u8>, SocketAddr)>(32);
 
     for speak_addr in speak_addresses {
-        let mut buf: [u8; 1024] = [0; 1024];
-
         let tx = tx.clone();
         let mut rx = tx.subscribe();
 
@@ -39,6 +77,7 @@ async fn main() -> io::Result<()> {
         let speak_sock = listen_sock.clone();
 
         tokio::spawn(async move {
+            let mut buf: [u8; DATAGRAM_SIZE] = [0; DATAGRAM_SIZE];
             let (len, source_addr) = listen_sock.recv_from(&mut buf).await.unwrap();
             println!("Got {} bytes from {:?}", len, source_addr);
             // TODO: better comment
